70945b7830372a1156e210253ded44b8cb064794
yashveer singh <120788124+yash314314@users.noreply.github.com>
2026-01-02 17:32:06 +0530

    api updated baseurl

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000', 
+  baseURL: 'https://chatbot-ubjh.onrender.com', 
 });
 
 api.interceptors.request.use((config) => {
