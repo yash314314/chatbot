1d3a27c86af0c01cccf570d5439044441f7a606b
yashveer singh <120788124+yash314314@users.noreply.github.com>
2026-01-02 16:44:34 +0530

    databse update

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,12 +2,30 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+import ssl
 from dotenv import load_dotenv
 
 load_dotenv()
+
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_recycle=3600)
+connect_args = {}
+
+if SQLALCHEMY_DATABASE_URL and "aiven" in SQLALCHEMY_DATABASE_URL:
+
+    ssl_context = ssl.create_default_context()
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_NONE
+    
+    connect_args = {
+        "ssl": ssl_context
+    }
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, 
+    pool_recycle=3600,
+    connect_args=connect_args
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
